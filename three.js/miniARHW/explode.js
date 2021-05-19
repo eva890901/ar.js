4be63450e4a6e18c4ee1535449f4d1ba6a59f280
eva890901 <a0983981638@gmail.com>
2021-05-20 03:05:57 +0800

    HW5

--- three.js/miniARHW/explode.js
+++ three.js/miniARHW/explode.js
@@ -23,41 +23,42 @@ function buildSprite(texMat, markerGroup) {
     geometry.computeVertexNormals();
 
     sprite = new THREE.Mesh(geometry, texMat);
+	sprite.rotation.x = Math.PI/2;
     markerGroup.add(sprite);
 }
 
 function initSprite(markerGroup) {
-  setUpOffsetArray();
+	setUpOffsetArray();
 
-  // instantiate a loader
-  var loader = new THREE.TextureLoader();
-  loader.setCrossOrigin ('');
-  // load a resource
-  loader.load(
-    // resource URL
-    'https://i.imgur.com/6ePTx6p.png',
+	// instantiate a loader
+	var loader = new THREE.TextureLoader();
+	loader.setCrossOrigin ('');
+	// load a resource
+	loader.load(
+	// resource URL
+	'https://i.imgur.com/6ePTx6p.png',
 
-    // Function when resource is loaded
-    function(texture) {
-      // Plane with default texture coordinates [0,1]x[0,1]
-      var texMat = new THREE.MeshBasicMaterial({
-        map: texture,
-        transparent: true, // cutout texture: set transparent: true
-        side: THREE.DoubleSide
-      });
-      texture.wrapS = THREE.RepeatWrapping;
-      texture.wrapT = THREE.RepeatWrapping;
-      buildSprite(texMat, markerGroup);
-    },
-    // Function called when download progresses
-    function(xhr) {
-      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    // Function called when download errors
-    function(xhr) {
-      console.log('An error happened');
-    }
-  );
+	// Function when resource is loaded
+	function(texture) {
+	// Plane with default texture coordinates [0,1]x[0,1]
+		var texMat = new THREE.MeshBasicMaterial({
+			map: texture,
+			transparent: true, // cutout texture: set transparent: true
+			side: THREE.DoubleSide
+		});
+		texture.wrapS = THREE.RepeatWrapping;
+		texture.wrapT = THREE.RepeatWrapping;
+		buildSprite(texMat, markerGroup);
+	},
+	// Function called when download progresses
+	function(xhr) {
+		console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+	},
+	// Function called when download errors
+	function(xhr) {
+		console.log('An error happened');
+	}
+	);
 
 }
 
@@ -67,41 +68,40 @@ function initSprite(markerGroup) {
 // how the array going to increment in this state 
 // initial uv (of lower left corner) is (0, 0.75)
 function setUpOffsetArray() {
-  uvOffsetArray = [];
-  var rowCount = 4; // 4x8 sprites
-  var colCount = 8;
-  for (var i = 0; i < rowCount; i++) {
-    var row = [];
-    for (var j = 0; j < colCount; j++)
-      row.push(new THREE.Vector2(j * 0.125, - 0.25 * i));
-    uvOffsetArray.push(row);
-  }
+	uvOffsetArray = [];
+	var rowCount = 4; // 4x8 sprites
+	var colCount = 8;
+	for (var i = 0; i < rowCount; i++) {
+		var row = [];
+		for (var j = 0; j < colCount; j++)
+			row.push(new THREE.Vector2(j * 0.125, - 0.25 * i));
+		uvOffsetArray.push(row);
+	}
 }
 
 function _spriteAnimate() {
- let msg = `[${baseS}] [${baseT}]`
- console.log (msg);
-  
-  sprite.material.map.offset.copy (uvOffsetArray[baseS][baseT]);  
-  baseT = (baseT + 1) % 8;
-  if (baseT === 0) {
-    baseS = (baseS + 1) % 4;
-  }
-  
-  if (baseS !== 3 || baseT !== 7) // NOT (baseS = 3 ^ baseT = 7)
-  	setTimeout (_spriteAnimate, 100); // proceed to next frame
-  else {
-  	sprite.material.map.offset.copy (uvOffsetArray[0][0]);  // back to first frame
-  }
+	let msg = `[${baseS}] [${baseT}]`
+
+	sprite.material.map.offset.copy (uvOffsetArray[baseS][baseT]);  
+	baseT = (baseT + 1) % 8;
+	if (baseT === 0) {
+		baseS = (baseS + 1) % 4;
+	}
+
+	if (baseS !== 3 || baseT !== 7) // NOT (baseS = 3 ^ baseT = 7)
+		setTimeout (_spriteAnimate, 100); // proceed to next frame
+	else {
+		sprite.material.map.offset.copy (uvOffsetArray[0][0]);  // back to first frame
+	}
 }
 
 // trigger the animation
 function spriteAnimate() {
-  if (sprite === undefined) {
-  	console.log ('return in spriteAnimate')
-  	return;
-  }
-  
-  baseS = baseT = 0;
-  setTimeout (_spriteAnimate, 100);
+	if (sprite === undefined) {
+		console.log ('return in spriteAnimate')
+		return;
+	}
+
+	baseS = baseT = 0;
+	setTimeout (_spriteAnimate, 100);
 }
